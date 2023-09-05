08fcc64c2b4fa4d0ac54d7a57afadb6175105246
Студент <@DESKTOP-PBCUDRI>
2023-09-05 21:08:08 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int rollDice() {
+    return rand() % 6 + 1;
+}
+
+int main() {
+    setlocale(LC_ALL, "");
+    srand(time(0));
+
+    cout << "Кости" << endl;
+
+    char playAgain;
+
+    do {
+        int dice1 = rollDice();
+        int dice2 = rollDice();
+
+        cout << "Вам выпало число " << dice1 << " и число " << dice2 << endl;
+
+        cout << "Бросить кости снова?? (y/n): ";
+        cin >> playAgain;
+    } while (playAgain == 'y' || playAgain == 'Y');
+
+    return 0;
+}
